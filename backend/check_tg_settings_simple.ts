314a4dbc67a0f8ea6df2d5d20ee8c696710a54f3
eladnaim <naimelad1@gmail.com>
2026-02-03 19:28:48 +0200

    Security: Removed sensitive debug scripts and updated .gitignore

--- backend/check_tg_settings_simple.ts
+++
@@ -1,8 +0,0 @@
-import { db } from './db.js';
-
-async function checkTelegram() {
-    const doc = await db.collection('settings').doc('telegram').get();
-    console.log(JSON.stringify(doc.data(), null, 2));
-}
-
-checkTelegram();
